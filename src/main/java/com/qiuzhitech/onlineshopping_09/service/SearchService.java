17c3c45c1b8422f92074e900bfb04dd4361ff1bd
PzGallium <zijia.pan@gmail.com>
2026-06-12 23:41:23 -0400

    Add searchCommodityByES

--- src/main/java/com/qiuzhitech/onlineshopping_09/service/SearchService.java
+++ src/main/java/com/qiuzhitech/onlineshopping_09/service/SearchService.java
@@ -1,19 +1,29 @@
 package com.qiuzhitech.onlineshopping_09.service;
 
+import com.qiuzhitech.onlineshopping_09.config.ESConfig;
 import com.qiuzhitech.onlineshopping_09.db.dao.OnlineShoppingCommodityDao;
 import com.qiuzhitech.onlineshopping_09.db.po.OnlineShoppingCommodity;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class SearchService {
     @Resource
     OnlineShoppingCommodityDao onlineShoppingCommodityDao;
+    @Resource
+    ESService esService;
+
 
     public List<OnlineShoppingCommodity> searchCommodityByDB(String keyword) {
         return onlineShoppingCommodityDao.searchCommodityByKeyword(keyword);
+    }
+
+    public List<OnlineShoppingCommodity> searchCommodityByES(String keyword) {
+        return esService.searchCommodityByES(keyword);
     }
 }
 
