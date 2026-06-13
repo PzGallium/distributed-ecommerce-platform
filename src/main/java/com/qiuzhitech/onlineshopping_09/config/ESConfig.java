630a41ffe405b255836d2921e10d424102dc34e2
PzGallium <zijia.pan@gmail.com>
2026-06-12 23:38:21 -0400

    ESCongig

---
+++ src/main/java/com/qiuzhitech/onlineshopping_09/config/ESConfig.java
@@ -0,0 +1,18 @@
+package com.qiuzhitech.onlineshopping_09.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ESConfig {
+    @Bean
+    public RestHighLevelClient EsClientProvider() {
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",
+                        9200,"http" ))
+        );
+    }
+}
