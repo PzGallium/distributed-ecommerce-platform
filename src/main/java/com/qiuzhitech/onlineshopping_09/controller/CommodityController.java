273a375cf5f11186b38e9efccf463d3a0d3f088c
PzGallium <zijia.pan@gmail.com>
2026-06-18 23:33:47 -0400

    Set up @PostConstruct method to generate a rule to limit qps and add a new method itemListBySeller with the rule.

--- src/main/java/com/qiuzhitech/onlineshopping_09/controller/CommodityController.java
+++ src/main/java/com/qiuzhitech/onlineshopping_09/controller/CommodityController.java
@@ -1,15 +1,25 @@
 package com.qiuzhitech.onlineshopping_09.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.EntryType;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import com.qiuzhitech.onlineshopping_09.db.dao.OnlineShoppingCommodityDao;
 import com.qiuzhitech.onlineshopping_09.db.po.OnlineShoppingCommodity;
 import com.qiuzhitech.onlineshopping_09.service.SearchService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
+@Slf4j
 @Controller
 public class CommodityController {
     @Resource
@@ -59,6 +69,37 @@ public class CommodityController {
         List<OnlineShoppingCommodity> commodities = onlineShoppingCommodityDao.listItems();
         resultMap.put("itemList", commodities);
         return "list_items";
+    }
+
+    @GetMapping("/commodities/{sellerID}")
+    String itemListBySeller(@PathVariable("sellerID") long sellerID, Map<String, Object> resultMap) {
+        try (Entry entry = SphU.entry("listItemsRule", EntryType.IN, 1,
+                sellerID)) {
+            List<OnlineShoppingCommodity> commodities = onlineShoppingCommodityDao.selectByUserId(sellerID);
+            resultMap.put("itemList", commodities);
+        } catch (BlockException e) {
+            log.error("ListItems got throttled" + e.toString());
+            return "wait";
+        }
+        return "list_items";
+    }
+
+    @PostConstruct
+    public void init() {
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        // Define resource
+        rule.setResource("listItemsRule");
+        rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        //Define QPS count
+        rule.setCount(1);
+        // FlowRule rule2 = new FlowRule();
+        // rule2.setGrade(RuleConstant.FLOW_GRADE_QPS);
+        // rule2.setCount(2);
+        // rule2.setResource("HelloResource");
+        rules.add(rule);
+        // rules.add(rule2);
+        FlowRuleManager.loadRules(rules);
     }
 
 
